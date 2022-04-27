b374cb2d20a04895b4743f1846053bea2435afb4
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-04-27 11:37:17 +0900

    02_04_確認課題

--- main.cpp
+++ main.cpp
@@ -363,6 +363,7 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE; // カリングしない
 	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID; // ポリゴン内塗りつぶし
 	pipelineDesc.RasterizerState.DepthClipEnable = true; // 深度クリッピングを有効に
+	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME; //ワイヤーフレーム
 
 	// ブレンドステート
 	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask
@@ -464,9 +465,9 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 		
 		// ビューポート設定コマンド
 		D3D12_VIEWPORT viewport{};
-		viewport.Width = window_width;
+		viewport.Width = window_width / 2;
 		viewport.Height = window_height;
-		viewport.TopLeftX = 0;
+		viewport.TopLeftX = 5;
 		viewport.TopLeftY = 0;
 		viewport.MinDepth = 0.0f;
 		viewport.MaxDepth = 1.0f;
@@ -476,9 +477,9 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 		// シザー矩形
 		D3D12_RECT scissorRect{};
 		scissorRect.left = 0; // 切り抜き座標左
-		scissorRect.right = scissorRect.left + window_width; // 切り抜き座標右
+		scissorRect.right = scissorRect.left + window_width ; // 切り抜き座標右
 		scissorRect.top = 0; // 切り抜き座標上
-		scissorRect.bottom = scissorRect.top + window_height; // 切り抜き座標下
+		scissorRect.bottom = scissorRect.top + window_height/2; // 切り抜き座標下
 		// シザー矩形設定コマンドを、コマンドリストに積む
 		commandList->RSSetScissorRects(1, &scissorRect);
 
