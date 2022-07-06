91a632dd2f539ee09856703aeb306d6a85f75811
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-07-06 11:20:40 +0900

    06-02_授業内容

--- Basic.hlsli
+++ Basic.hlsli
@@ -10,6 +10,9 @@ struct VSOutput
 	//システム用頂点座標
 	float4 svpos : SV_POSITION;
 
+	//法線ベクトル
+	float3 normal:NORMAL;
+
 	//UV値
 	float2 uv : TEXCOORD;
 
--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,10 +1,11 @@
 #include "Basic.hlsli"
 
 
-VSOutput main(float4 pos:POSITION, float2 uv : TEXCOORD)
+VSOutput main(float4 pos:POSITION,float3 normal:NORMAL, float2 uv : TEXCOORD)
 {
 	VSOutput output;//ピクセルシェーダに渡す値
 	output.svpos = mul(mat, pos);//座標に行列を乗算
 	output.uv = uv;
+	output.normal = normal;
 	return output;
 }
